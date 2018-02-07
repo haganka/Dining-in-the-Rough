c11d1b26894a23e935c990517e6db1cb0be2dce3
Kelly <kelly.hagan3@gmail.com>
2018-02-07 16:29:13 -0600

    adds carousel with rotating images and adjustments to search bar

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -231,3 +231,20 @@ function runQuery(latLong) {
         }
     });
 }
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 10000); // Change image every 2 seconds
+};
+
+$('.carousel').carousel();
